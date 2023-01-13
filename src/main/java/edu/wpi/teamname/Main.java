79551cb183e2dea35629ab4f21e8bfe192072348
kr0man0va <67516645+kr0man0va@users.noreply.github.com>
2023-01-13 14:44:41 -0500

    Added my name

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -3,9 +3,9 @@ package edu.wpi.teamname;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Kseniia");
     App.launch(App.class, args);
     System.out.println("David");
     System.out.println("Aidan Syrgak");
+    System.out.println("Kseniia");
   }
 }
