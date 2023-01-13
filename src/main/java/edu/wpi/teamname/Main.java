ab87c02cbf9e7c747dd8417100311d477a664c2c
sjdavid <sjdavid@wpi.edu>
2023-01-13 14:45:55 -0500

    E

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
   public static void main(String[] args) {
     App.launch(App.class, args);
+    System.out.println("Sam");
     System.out.println("David");
     System.out.println("Aidan Syrgak");
     System.out.println("Kseniia");
